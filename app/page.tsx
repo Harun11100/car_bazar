cf08720b4c56a53d4028f50f25642a7336a7bc9a
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-01 06:44:48 +0600

    Initial Initial files are added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 
 export default function Home() {
   return (
-    <div className="text-5xl text-blue-500">Hello there !</div>
+    <main className="b">
+          <div className="text-5xl text-blue-500">Hello there !</div>
+    </main>
+   
   );
 }
