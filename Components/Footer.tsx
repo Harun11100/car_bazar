a166a5d7c5dfe4d25d36370cd6b5b99298a609ba
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-02 07:07:29 +0600

    Footer section is completely done

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,18 +1,53 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-   <footer className='max-w-[1140px] px-5 md:px-10 mx-auto pb-10'>
-     
-      <div>
+   <footer className='footer px-10'>
+     <div className='footer__links-container'>
+      <div className='px-16  pb-10'>
             <Image src='/logo.png'  alt='logo'
-            height={100} width={100}/>
+            height={100} width={100}
+            className=''/>
+            <p >CarBazar 2025<br/>
+            All right reserved ©</p>
       </div>
-       <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fuga expedita, esse laboriosam exercitationem quae veritatis illo molestiae dolore nihil recusandae excepturi, consequatur nulla deserunt culpa vitae officia et quo.</p>
-       </div>
+      
+         <div className='footer__links'>
+             {footerLinks.map(link=>
+               <div key={link.title} className='footer__link'>
+                  <h3 className='font-bold'>{link.title}</h3>
+                  {link.links.map(item=>(
+                     <Link key={item.title} href={item.url}>
+                        
+                        {item.title}
+                         
+                     </Link>
+                  ))}
 
+               </div>
+             )}
+         </div>
+         <div>
+
+         </div>
+         </div>
+         <div className='footer__copyrights'>
+         <p >CarBazar 2025
+         All right reserved ©</p>
+         <div className='footer__copyrights-link'>
+            <Link href="/" >
+               Privacy policy
+            </Link>
+            <Link href="/" >
+              Terms & conditions
+            </Link>
+         </div>
+
+         </div>
+    
    </footer>
   )
 }
