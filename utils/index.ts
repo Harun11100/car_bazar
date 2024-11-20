347701118d89907f3903572468512cdc3cf86a38
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-20 22:59:41 +0600

    Faceing Serversite rendering problem through API

--- utils/index.ts
+++ utils/index.ts
@@ -17,7 +17,7 @@ export async function fetchCars(filter:FilterProps){
 
 
 const headers:HeadersInit={
-'x-rapidapi-key': '6cdaa68426msh9fbe734a4c1ba77p1fbbf4jsn79e14363a607',
+'x-rapidapi-key':'6cdaa68426msh9fbe734a4c1ba77p1fbbf4jsn79e14363a607',
     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 }
 
